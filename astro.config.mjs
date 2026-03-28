ddf6e6ca3511b89e203c520666b8595e9fe8ea55
kohpika <kohdekiru@gmail.com>
2026-03-28 13:51:30 +0900

    add OGP

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://https://diary-blog-sage.vercel.app/",
   vite: {
     plugins: [tailwindcss()]
   }

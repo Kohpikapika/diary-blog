2c8d4f9c8c60b35a914bed48d8f7607327f4d348
kohpika <kohdekiru@gmail.com>
2026-05-03 23:28:07 +0900

    add tailwindcss/typography

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{astro,js,ts,jsx,tsx,md}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+  ],
+};
